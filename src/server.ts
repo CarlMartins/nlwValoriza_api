d001abc3d92a709eef653b9982ee4d8d2940888c
Carlos Martins <lemos_oliveiraa@hotmail.com>
2021-06-23 10:24:22 -0300

    Middleware de tratamento de erros e criacao de tags

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Request, Response, NextFunction, response } from 'express';
+import "express-async-errors";
 import { router } from './routes/routes';
+import { errorHandler } from './middlewares/errorHandler';
 import './database';
 
 const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('Server is running http://localhost:3000');
