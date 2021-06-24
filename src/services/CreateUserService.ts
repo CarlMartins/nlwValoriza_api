150549e4530a119ff1818e49017c636da81eb64b
Carlos Martins <lemos_oliveiraa@hotmail.com>
2021-06-24 16:15:09 -0300

    Adicionado criptografia na senha

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,5 +1,6 @@
 import { getCustomRepository } from 'typeorm';
 import { UserRepositories } from "../repositories/UserRepositories";
+import { hash } from 'bcryptjs';
 
 interface IUserRequest {
   name: string;
@@ -9,7 +10,7 @@ interface IUserRequest {
 }
 
 class CreateUserService {
-  async execute({ name, email, admin, password }: IUserRequest) {
+  async execute ({ name, email, admin, password }: IUserRequest) {
     const usersRepository = getCustomRepository(UserRepositories);
 
     if (!email) {
@@ -24,11 +25,13 @@ class CreateUserService {
       throw new Error("User already exists");
     }
 
+    const passwordHash = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
       admin,
-      password
+      password: passwordHash
     });
 
     usersRepository.save(user);
