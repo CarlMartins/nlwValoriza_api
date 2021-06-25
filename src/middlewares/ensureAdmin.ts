103326f518be2fca3c03fc22806099be8a8827e8
Carlos Martins <lemos_oliveiraa@hotmail.com>
2021-06-25 11:07:27 -0300

    Problema com referencia a admin

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,10 +1,13 @@
 import { Request, Response, NextFunction } from "express";
+import { getCustomRepository } from "typeorm";
+import { UserRepositories } from "../repositories/UserRepositories";
 
-export function ensureAdmin (req: Request, res: Response, next: NextFunction) {
+export async function ensureAdmin (req: Request, res: Response, next: NextFunction) {
   const { user_id } = req;
-  console.log(user_id);
 
-  const admin = true;
+  const usersRepositories = getCustomRepository(UserRepositories);
+
+  const { admin } = await usersRepositories.findOne(user_id);
 
   if (admin) {
     return next();
